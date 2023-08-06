a7982d542ed90a72930fd15f6a461e8421b8eaad
aph1d0 <filip.smolinski037@gmail.com>
2023-08-06 21:27:54 +0200

    adding timeout and less workers

--- gunicorn_starter.sh
+++ gunicorn_starter.sh
@@ -3,4 +3,4 @@
 
 # Here we will be spinning up multiple threads with multiple worker processess(-w) and perform a binding.
 
-gunicorn serwis_crm:"create_app()" -w 4 --threads 4 -b 0.0.0.0:80
+gunicorn serwis_crm:"create_app()" -w 2 --threads 2 -b 0.0.0.0:80 --timeout 1000
