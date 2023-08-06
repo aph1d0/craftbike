32c829e5a3bbee47c9c6dd8332b88f764a45f5b0
aph1d0 <filip.smolinski037@gmail.com>
2023-08-06 21:37:02 +0200

    fixing mysql port

--- serwis_crm/config.py
+++ serwis_crm/config.py
@@ -11,18 +11,18 @@ class Config(object):
 class DevelopmentConfig(Config):
     DEBUG = True
     SECRET_KEY = os.getenv("DEV_SECRET_KEY")
-    SQLALCHEMY_DATABASE_URI = f'mysql://{os.getenv("DEV_MYSQL_USER")}:{os.getenv("DEV_MYSQL_PASS")}@{os.getenv("DEV_MYSQL_HOST")}/{os.getenv("DEV_MYSQL_DB_NAME")}'
+    SQLALCHEMY_DATABASE_URI = f'mysql://{os.getenv("DEV_MYSQL_USER")}:{os.getenv("DEV_MYSQL_PASS")}@{os.getenv("DEV_MYSQL_HOST")}:{os.getenv("DEV_MYSQL_PORT")}/{os.getenv("DEV_MYSQL_DB_NAME")}'
 
 
 class TestConfig(Config):
     TESTING = True
     SECRET_KEY = os.getenv("TEST_SECRET_KEY")
-    SQLALCHEMY_DATABASE_URI = f'mysql://{os.getenv("TEST_MYSQL_USER")}:{os.getenv("TEST_MYSQL_PASS")}@{os.getenv("TEST_MYSQL_HOST")}/{os.getenv("TEST_MYSQL_DB_NAME")}'
+    SQLALCHEMY_DATABASE_URI = f'mysql://{os.getenv("TEST_MYSQL_USER")}:{os.getenv("TEST_MYSQL_PASS")}@{os.getenv("TEST_MYSQL_HOST")}:{os.getenv("TEST_MYSQL_PORT")}/{os.getenv("TEST_MYSQL_DB_NAME")}'
 
 
 class ProductionConfig(Config):
     SECRET_KEY = os.getenv("SECRET_KEY")
-    SQLALCHEMY_DATABASE_URI = f'mysql://{os.getenv("MYSQL_USER")}:{os.getenv("MYSQL_PASS")}@{os.getenv("MYSQL_HOST")}/{os.getenv("MYSQL_DB_NAME")}'
+    SQLALCHEMY_DATABASE_URI = f'mysql://{os.getenv("MYSQL_USER")}:{os.getenv("MYSQL_PASS")}@{os.getenv("MYSQL_HOST")}:{os.getenv("MYSQL_PORT")}/{os.getenv("MYSQL_DB_NAME")}'
 
 
 
