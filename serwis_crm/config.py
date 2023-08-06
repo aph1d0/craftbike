278e0d22249afa009fa1debbcc9f7ce470b830aa
aph1d0 <filip.smolinski037@gmail.com>
2023-08-06 20:43:30 +0200

    fixing secret key

--- serwis_crm/config.py
+++ serwis_crm/config.py
@@ -10,18 +10,18 @@ class Config(object):
 
 class DevelopmentConfig(Config):
     DEBUG = True
-    SECRET_KEY = DEV_SECRET_KEY
+    SECRET_KEY = os.environ["DEV_SECRET_KEY"]
     SQLALCHEMY_DATABASE_URI = f'mysql://{os.environ["DEV_MYSQL_USER"]}:{os.environ["DEV_MYSQL_PASS"]}@{os.environ["DEV_MYSQL_HOST"]}/{os.environ["DEV_MYSQL_DB_NAME"]}'
 
 
 class TestConfig(Config):
     TESTING = True
-    SECRET_KEY = TEST_SECRET_KEY
+    SECRET_KEY = os.environ["TEST_SECRET_KEY"]
     SQLALCHEMY_DATABASE_URI = f'mysql://{os.environ["TEST_MYSQL_USER"]}:{os.environ["TEST_MYSQL_PASS"]}@{os.environ["TEST_MYSQL_HOST"]}/{os.environ["TEST_MYSQL_DB_NAME"]}'
 
 
 class ProductionConfig(Config):
-    SECRET_KEY = SECRET_KEY
+    SECRET_KEY = os.environ["SECRET_KEY"]
     SQLALCHEMY_DATABASE_URI = f'mysql://{os.environ["MYSQL_USER"]}:{os.environ["MYSQL_PASS"]}@{os.environ["MYSQL_HOST"]}/{os.environ["MYSQL_DB_NAME"]}'
 
 
